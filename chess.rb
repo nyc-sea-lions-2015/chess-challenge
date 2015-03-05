96f70195b9b8a23f3528e6344c3bace16332cf6a
Eveanandi Butler & Angel Baek & Malcolm Bouzi & Stephanie Lo <pair+ebutler90.gellieb.mbouzi.steppinlo@devbootcamp.com>
2015-03-05 12:44:01 -0500

    working on bishop functionality - DIAGONALSgit add .git add .

--- chess.rb
+++ chess.rb
@@ -28,6 +28,7 @@ class King < Piece
 
   def moves
     [*-1..1].permutation(2).to_a.each do |dx,dy|
+      next if dx == 0 && dy == 0
       @moves << [@x+dx, @y+dy]
     end
       @moves
@@ -68,14 +69,55 @@ class Rook < Piece
 
 end
 
+class Bishop < Piece
+  def initialize(x, y)
+    @x, @y = x, y
+    @moves = []
+  end
+
+  def moves
+    arr = [1, 1, -1, -1].permutation(2).to_a.uniq
+    arr.each do |dx, dy|
+        x = @x
+        y = @y
+      WIDTH-1-@x.times do |num|
+        next if @x > WIDTH-1 || @x < 0 || @y > WIDTH-1 || @y < 0
+        x += dx
+        y += dy
+        puts "x: #{@x}"
+        puts "y: #{@y}"
+        @moves << [x, y]
+      end
+
+      WIDTH-1-@x.times do |num|
+        x = @x
+        y = @y
+        next if @x > WIDTH-1 || @x < 0 || @y > WIDTH-1 || @y < 0
+        x += dx
+        y += dy
+        puts "second loop"
+        puts "x: #{@x}"
+        puts "y: #{@y}"
+        @moves << [x, y]
+      end
+    end
+    @moves
+  end
+
+
+end
+
 # king = King.new(5,3)
-# p king.captured!
+# p king.moves
 
 # knight = Knight.new(5,3)
 # p knight.moves
 
-rook = Rook.new(5,5)
-p rook.moves
+# rook = Rook.new(5,5)
+# p rook.moves
+
+bishop = Bishop.new(4,3)
+p bishop.moves
 
 
 
