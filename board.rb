f086dd7916f32496ba8ac86bd0c649f34fb74613
Eveanandi Butler & Angel Baek & Malcolm Bouzi & Stephanie Lo <pair+ebutler90.gellieb.mbouzi.steppinlo@devbootcamp.com>
2015-03-06 11:25:53 -0500

    board placement of pieces. pieces are not objects yet.

--- board.rb
+++ board.rb
@@ -1,6 +1,8 @@
+WIDTH = 8
 class Board
   def initialize
-    @board = Array.new(8) {8.times.map{[]}}
+    @board = Array.new(WIDTH) {WIDTH.times.map{[]}}
+    @removed = []
   end
 
   def clear
@@ -9,25 +11,55 @@ class Board
 
   def start
     #TODO: place pawns in appropriate places
+    @board.map.with_index do |row, i|
+      if i== 1 || i == 6
+        row.map do |square|
+          square << "PIECE"
+        end
+      end
+      if i == 0 || i == WIDTH-1
+        row.map.with_index do |square,square_i|
+          if square_i == 0 || square_i == WIDTH-1
+            square << "ROOK"
+          elsif square_i == 1 || square_i == WIDTH-2
+            square << "KNIGHT"
+          elsif square_i ==2 || square_i == WIDTH-3
+            square << "BISHOP"
+          elsif square_i == 3
+            square << "QUEEN"
+          else
+            square << "KING"
+          end
+        end
+      end
+    # end
+    end
+    @board
   end
 
   def place(piece, position)
     #assume piece = some instance of a piece-type class
     #assume position is an array [x,y]
-    piece.arguments = position
+    # piece = Pawn.new(position)
+    # piece.arguments = position
   end
 
-  def removed
-
+  def remove
+    #TODO: remove pieces if piece.captured == true
+    #stores removed pieces in removed array.
   end
 
   def to_s
     @board.each_with_index.map do |row, i|
-      puts "#{8-i} #{row}"
+      # puts "#{8-i} #{row}"
+      puts "#{i} #{row}"
+
     end
     "   " + [*"a".."h"].join("   ")
   end
 end
 
 board = Board.new
+# puts board
+board.start
 puts board
