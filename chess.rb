3f9fdb72d45ebcdc0562538bb803e96f7802c602
Ben Cheng & Ryan Blecher & Ryan <pair+bcheng90.notryanb.rimmesbe@devbootcamp.com>
2015-03-06 12:02:29 -0500

    Add logic for obstructions in Rook and partial Queen

--- chess.rb
+++ chess.rb
@@ -91,27 +91,85 @@ class Rook < Piece
   end
 
   def movement(board)
-    #horizontal
-    (-7..7).each do |dy|
+    #up
+    -1.downto(-7) do |dy|
       y_pos = position[0] - dy
       # puts y_pos
       if y_pos.between?(0,7)
-        if board[y_pos][position[1]] == "-" || board[y_pos][position[1]].color != self.color
+        if board[y_pos][position[1]] == "-"
           @possible_moves << [y_pos, position[1]]
-        end
-      end
-    end
-    #vertical
-    (-7..7).each do |dx|
-      x_pos = position[1] - dx
-      # puts y_pos
-      if x_pos.between?(0,7)
-        if board[position[0]][x_pos] == "-" || board[position[0]][x_pos].color != self.color
-          @possible_moves << [position[1], x_pos]
+        else
+          # puts "Original: #{@color}"
+          # puts "Obstruction: #{board[y_pos][position[1]].color}"
+          if board[y_pos][position[1]].color != @color
+            @possible_moves << [y_pos, position[1]]
+            break
+          else
+            # @possible_moves << [y_pos, position[1]]
+            break
+          end
         end
       end
     end
 
+    #down
+    1.upto(7) do |dy|
+      y_pos = position[0] - dy
+      if y_pos.between?(0,7)
+        if board[y_pos][position[1]] == "-"
+          @possible_moves << [y_pos, position[1]]
+        else
+          # puts "Original: #{@color}"
+          # puts "Obstruction: #{board[y_pos][position[1]].color}"
+          if board[y_pos][position[1]].color != @color
+            @possible_moves << [y_pos, position[1]]
+            break
+          else
+            # @possible_moves << [y_pos, position[1]]
+            break
+          end
+        end
+      end
+    end
+
+    #left
+    -1.downto(-7) do |dx|
+      x_pos = position[1] - dx
+      # puts y_pos
+      if x_pos.between?(0,7)
+        if board[position[0]][x_pos] == "-"
+          @possible_moves << [position[0], x_pos]
+        else
+          if board[position[0]][x_pos].color == @color
+            # @possible_moves << [y_pos, position[1]]
+            break
+          else
+            @possible_moves << [position[0], x_pos]
+            break
+          end
+        end
+      end
+    end
+
+    # #right
+    1.upto(7) do |dx|
+      x_pos = position[1] - dx
+      # puts y_pos
+      if x_pos.between?(0,7)
+        if board[position[0]][x_pos] == "-"
+          @possible_moves << [position[0], x_pos]
+        else
+          if board[position[0]][x_pos].color == @color
+            # @possible_moves << [y_pos, position[1]]
+            break
+          else
+            @possible_moves << [position[0], x_pos]
+            break
+          end
+        end
+      end
+    end
+  @possible_moves
   end
 end
 
@@ -182,24 +240,81 @@ class Queen < Piece
   end
 
   def movement(board)
-    #horizontal
-    (-7..7).each do |dy|
+    #up
+    -1.downto(-7) do |dy|
       y_pos = position[0] - dy
       # puts y_pos
       if y_pos.between?(0,7)
-        if board[y_pos][position[1]] == "-" || board[y_pos][position[1]].color != self.color
+        if board[y_pos][position[1]] == "-"
           @possible_moves << [y_pos, position[1]]
+        else
+          # puts "Original: #{@color}"
+          # puts "Obstruction: #{board[y_pos][position[1]].color}"
+          if board[y_pos][position[1]].color != @color
+            @possible_moves << [y_pos, position[1]]
+            break
+          else
+            # @possible_moves << [y_pos, position[1]]
+            break
+          end
         end
       end
     end
 
-    #vertical
-    (-7..7).each do |dx|
+    #down
+    1.upto(7) do |dy|
+      y_pos = position[0] - dy
+      if y_pos.between?(0,7)
+        if board[y_pos][position[1]] == "-"
+          @possible_moves << [y_pos, position[1]]
+        else
+          # puts "Original: #{@color}"
+          # puts "Obstruction: #{board[y_pos][position[1]].color}"
+          if board[y_pos][position[1]].color != @color
+            @possible_moves << [y_pos, position[1]]
+            break
+          else
+            # @possible_moves << [y_pos, position[1]]
+            break
+          end
+        end
+      end
+    end
+
+    #left
+    -1.downto(-7) do |dx|
       x_pos = position[1] - dx
       # puts y_pos
       if x_pos.between?(0,7)
-        if board[position[0]][x_pos] == "-" || board[position[0]][x_pos].color != self.color
-          @possible_moves << [position[1], x_pos]
+        if board[position[0]][x_pos] == "-"
+          @possible_moves << [position[0], x_pos]
+        else
+          if board[position[0]][x_pos].color == @color
+            # @possible_moves << [y_pos, position[1]]
+            break
+          else
+            @possible_moves << [position[0], x_pos]
+            break
+          end
+        end
+      end
+    end
+
+    # #right
+    1.upto(7) do |dx|
+      x_pos = position[1] - dx
+      # puts y_pos
+      if x_pos.between?(0,7)
+        if board[position[0]][x_pos] == "-"
+          @possible_moves << [position[0], x_pos]
+        else
+          if board[position[0]][x_pos].color == @color
+            # @possible_moves << [y_pos, position[1]]
+            break
+          else
+            @possible_moves << [position[0], x_pos]
+            break
+          end
         end
       end
     end
@@ -266,8 +381,12 @@ class ChessBoard
     # Populate new board by creating a 2D array with rows (0..7)
     @board = Array.new(8) {["-","-","-","-","-","-","-","-"]}
     # @board[4][4] = King.new("black", [4,4])
-    @board[4][4] = Queen.new("black", [4,4])
-    @board[3][3] = Bishop.new("black", [3,3])
+    # @board[4][4] = Queen.new("black", [4,4])
+    # @board[3][3] = Bishop.new("black", [3,3])
+    @board[4][4] = Rook.new("white", [4,4])
+    @board[2][4] = Bishop.new("black", [2,4])
+    @board[4][2] = Queen.new("white", [4,2])
+    @board[6][4] = Knight.new("white", [6,4])
 
   end
 
