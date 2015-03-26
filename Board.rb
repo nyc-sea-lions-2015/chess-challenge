0aa0e4ae34375479f53b8f36d7c3ac81039a5f28
Alexander Mihalopoulos <amihalop@gmail.com>
2015-03-26 19:05:17 -0400

    Add icons to pieces

--- Board.rb
+++ Board.rb
@@ -1,9 +1,11 @@
-classes
 
 class Board(args)
-  2
   def initialize
-    @board = args
+    @board = [Array.new(8) { Array.new(nil) }]
+    @first_row = [Rook.new, Knight.new, Bishop.new, Queen.new, King.new, Bishop.new, Knight.new, Rook.new]
+    @second_row = Array.new(8) {Pawns.new}
+    @board[0], @board[7] = @first_row, @first_row.reverse.map { |piece| piece.color = "black"}
+    @board[1], @board[6] = @second_row, @second_row.map { |piece| piece.color = "black"}
   end
 
 
@@ -14,25 +16,29 @@ class Board(args)
 
    x_location = piece.location[0]
    y_location = piece.location[1]
-   x_difference = piece.moves[0]
-   y_difference = piece.moves[1]
-   # filter for out of bounds. out of bounds if x or y < 0 || x or y > 7
-   possibilities << [(x_location + x_difference), (y_location + y_difference)]
-    out_of_bounds?#mathmatical possibilities
+ (piece.moves).each do |vectors|
+
+    x.location += piece.moves[0]
+    y.location += piece.moves[1]
+
+    next if out_of_bounds?(x_location, y_location)
+     possibilities << [x_location, y_location]
+    #mathmatical possibilities
     #check against piece.location
 
     possibilites.each do |coordinate|
       # if collision with white piece, return false
-        if @board[coordinate[0]][coordinate[1]]
+      if @board[coordinate[0]][coordinate[1]]
 
-        else
-          valid_moves << coordinate
+      else
+        valid_moves << coordinate
 
 
     # move to 0,2 and check a bunch
     #
     # figure out which directions are out of bounds
-    #
+  def out_of_bounds?(x, y)
+    (x < 0 || y < 0 || x > 7 || y > 7)
   end
 
   def find_piece(location_string)
@@ -65,18 +71,21 @@ end
 
 class Piece
   attr_accessor :color, :moves, :location
-  def initialize(color)
-    @color = color
-    @moves = moves #possible moves on an empty board
-    @location = location
+  attr_reader :display
+  def initialize(color = "white")
+    color = "white" ? @icon =
+    # @color = color
+    # @moves = moves #possible moves on an empty board
+    # @location = location
+    @display_options = display[color]
   end
 
 
 end
 
 class Pawn < Piece
-  def initialize()
-
+  def initialize(color = "white")
+    color == "white" ? @icon = "♟" : @icon = '♙'
      self.moves = [0,1]
     # pawn can only move forward.
     # pawn can only move 1 space, unless it's current position is the starting col.
@@ -88,29 +97,41 @@ class Pawn < Piece
 
   # TODO: deal with possible_moves method
   class Rook < Piece
+    def initialize(color = "white")
+      color == "white" ? @icon = "♜" : @icon = '♖'
+    end
+
   end
-<<<<<<< HEAD
-
-
-class King < Piece
-  moves = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
-  possibilities = []
-end
-
-class Queen < Piece
-
-end
-=======
 
   class King < Piece
+    def initialize(color = "white")
+      color =="white" ? @icon = "♚" : @icon = '♔'
+      @moves = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
+    end
+
+    # def move_options(x, y)
+    #   @moves.map do |vector|
+    #     x += vector[0]
+    #     y += vector[1]
+    #   end
+    # end
   end
 
   class Queen < Piece
+    def initialize(color = "white")
+      color == "white" ? @icon = "♛" : @icon = '♕'
+    end
+
   end
->>>>>>> 6d730b1fe19c176e220e9f00edbe550a699293ce
 
   class Bishop < Piece
+    def initialize(color = "white")
+      color == "white" ? @icon = "♝" : @icon = '♗'
+    end
   end
 
   class Knight < Piece
+    def initialize(color = "white")
+      color == "white" ? @icon = "♞" : @icon = '♘'
+    end
   end
