4f06650730d0172e485318642dd11ba1a4d95f8f
Alexander Mihalopoulos <amihalop@gmail.com>
2015-03-27 11:26:43 -0400

    Add grant logic with old stuff commented

--- Board.rb
+++ Board.rb
@@ -36,6 +36,11 @@ class Board
   #   end
   #   puts board_string
   # end
+  def move(old_pos, new_pos, piece)
+    #capture(board[new_x][new_y]) if board[new_x][new_y] != nil
+    board[new_x][new_y] = piece
+    board[old_x][old_y] = nil
+  end
 
   def display
     row_num = 8
@@ -49,44 +54,109 @@ class Board
     end
   end
 
-  def valid_moves(piece)
-    valid_moves = []
-   x = piece.location[0]
-   y= piece.location[1]
-   possibilities = all_possible_directions(x, y)
-     #filter for out_of_bounds
-      #mathematical possibilities
-      #check against piece.location
+  #   def valid_moves(piece)
+  #     valid_moves = []
+  #    x = piece.location[0]
+  #    y= piece.location[1]
+  #    possibilities = all_possible_directions(x, y)
+  #      #filter for out_of_bounds
+  #       #mathematical possibilities
+  #       #check against piece.location
+  #     possibilites.each do |coordinate|
+  #       # if collision with white piece, return false
+  #         x = coordinate[0]
+  #         y = coordinate[1]
+  #         next if out_of_bounds?(x,y)
+  #         next if (@board[x][y]).color == piece.color
+  #         valid_moves << coordinate
+  #     end
+  #     valid_moves
+  # end
+
+  def valid_moves(valid_moves = [])
+    x = piece.location[0]
+    y= piece.location[1]
+    valid_moves = move_one(x, y) if valid_moves == [] #otherwise you get values from the recursive move check
+    #filter for out_of_bounds
+    #mathematical valid_moves
+    #check against piece.location
     possibilites.each do |coordinate|
       # if collision with white piece, return false
-        x = coordinate[0]
-        y = coordinate[1]
-        next if out_of_bounds?(x,y)
-        next if (@board[x][y]).color == piece.color
-        valid_moves << coordinate
+      x = coordinate[0]
+      y = coordinate[1]
+      next if out_of_bounds?(x,y)
+      next if (@board[x][y]).color == piece.color
+      valid_moves << coordinate
     end
     valid_moves
-end
+  end
 
-def all_possible_directions(x,y)
-    possibilities = []
+  def recursive_move_check(piece, check_x=0, check_y=0, valid_before_bounds_check=[])
+    directions = piece.moves
+    row = piece.location[0]
+    col = piece.location[1]
+    check_x += row         #incrementing out from current x location
+    check_y += col         #incrementing out from current y location
+    directions.each do |direction|
+      check_x += direction[0]
+      check_y += direction[1]
+      if free_space?(check_x, check_y)
+        valid_before_bounds_check << [check_x,check_y]
+        recursive_move_check(piece, check_x, check_y, valid_before_bounds_check)
+      else
+        return valid_moves(valid_before_bounds_check) #base case for if it runs into a guy
+      end
+    end
+  end
+
+  # def all_possible_directions(x,y)
+  #     possibilities = []
+  #     (piece.moves).each do |vector|
+  #       x += vectors[0]
+  #       y += vector[1]
+  #       possibilities <<  [x,y]
+  #     end
+  #     possibilities
+  # end
+
+  def free_space?(check_row, check_col)
+    @board[check_row][check_col] == nil
+  end
+
+  def move_one(x,y)
+    valid_moves = []
     (piece.moves).each do |vector|
       x += vectors[0]
       y += vector[1]
-      possibilities <<  [x,y]
+      valid_moves <<  [x,y]
     end
-    possibilities
-end
+    valid_moves
+  end
 
-def out_of_bounds?(x,y)
-  (x < 0 || y < 0 || x > 7|| y > 7)
-end
+
+  def out_of_bounds?(x,y)
+    (x < 0 || y < 0 || x > 7|| y > 7)
+  end
+
+  #   def find_piece(location_string)
+  #       index = string_to_index(location_string)
+  #       piece = @board[index[0]][index[1]]
+  #       piece.location = [index[0], index[1]]
+  #       valid_moves(piece)
+  #   end
+
+  #   def string_to_index(location_string)
+  #     # a5
+  #     col_string, row_index = location_string.split("")
+  #     row_index = BOARDLENGTH - row_index.to_i
+  #     col_index = col_string.downcase.ord - 97
+  #     [row_index, col_index]
+  #   end
+  # end
 
   def find_piece(location_string)
-      index = string_to_index(location_string)
-      piece = @board[index[0]][index[1]]
-      piece.location = [index[0], index[1]]
-      valid_moves(piece)
+    index = string_to_index(location_string)
+    piece = @board[index[0]][index[1]]
   end
 
   def string_to_index(location_string)
@@ -102,10 +172,11 @@ end
 
 
 class Piece
-  attr_accessor :color, :moves, :location
+  attr_accessor :color, :moves, :location, :name
   attr_reader :display
   def initialize(color = "white")
-     @icon = icon
+    @icon = icon
+    @all_adjacent = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
   end
   def display_icon
     @icon
@@ -114,55 +185,66 @@ end
 
 
 class Pawn < Piece
-  attr_accessor :moves
+  attr_accessor :moves #:first_move?, :capturing?
+  #logic for capturing?
   def initialize(color = "white")
     color == "white" ? @icon = "♟" : @icon = '♙'
-    @moves = [0, 1]
+    # first_move? == true if self.location[0] == 1 || self.location[0] == 6 #initial row value for pawns
+    moves = [0, 1]
+    # moves << [0,2] if first_move?
+    # moves << [1,1] if capturing?
+    name = "pawn"
   end
 
 end
 
-  # TODO: deal with possible_moves method
-  class Rook < Piece
-    attr_accessor :moves
-    def initialize(color = "white")
-      color == "white" ? @icon = "♜" : @icon = '♖'
-      @moves = [[0,1], [1,0], [-1,0], [0, -1]]
-    end
-
-
-  end
-
-  class King < Piece
-    def initialize(color = "white")
-      color =="white" ? @icon = "♚" : @icon = '♔'
-      @moves = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
-    end
+# TODO: deal with possible_moves method
+class Rook < Piece
+  attr_accessor :moves
+  def initialize(color = "white")
+    color == "white" ? @icon = "♜" : @icon = '♖'
+    @moves = [[0,1], [1,0], [-1,0], [0, -1]]
+    name = "rook"
   end
 
 
-  class Queen < Piece
-    def initialize(color = "white")
-      color == "white" ? @icon = "♛" : @icon = '♕'
-      @moves = [[1, 0], [1,1], [1, -1], [0, -1], [0, 1], [-1, 0], [-1, 1], [-1, -1]]
-    end
-  end
+end
 
-  class Bishop < Piece
-    def initialize(color = "white")
-      color == "white" ? @icon = "♝" : @icon = '♗'
-      @moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
-    end
+class King < Piece
+  def initialize(color = "white")
+    color =="white" ? @icon = "♚" : @icon = '♔'
+    @moves = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
+    name = "king"
   end
+end
 
-  class Knight < Piece
-    def initialize(color = "white")
-      color == "white" ? @icon = "♞" : @icon = '♘'
-      @moves = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
-    end
+
+class Queen < Piece
+  def initialize(color = "white")
+    color == "white" ? @icon = "♛" : @icon = '♕'
+    @moves = [[1, 0], [1,1], [1, -1], [0, -1], [0, 1], [-1, 0], [-1, 1], [-1, -1]]
+    name = "queen"
   end
+end
+
+class Bishop < Piece
+  def initialize(color = "white")
+    color == "white" ? @icon = "♝" : @icon = '♗'
+    @moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+    name = "bishop"
+  end
+end
+
+class Knight < Piece
+  def initialize(color = "white")
+    color == "white" ? @icon = "♞" : @icon = '♘'
+    @moves = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
+    name = "knight"
+  end
+end
 
 b = Board.new
 # p b.board
 
 puts b.display
+b.move()
