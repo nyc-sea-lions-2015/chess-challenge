5e6f922df8c7068ee5ce827fc9ad9be97dcd9b36
Alexander Mihalopoulos <amihalop@gmail.com>
2015-03-27 19:54:02 -0400

    Finish move check

--- Board.rb
+++ Board.rb
@@ -1,5 +1,5 @@
 
-# require 'byebug'
+
 class Board
   attr_accessor :board
   def initialize
@@ -55,16 +55,15 @@ class Board
   end
 
   def valid_move(piece)
+    p piece.moves
     valid_moves = []
     piece_moves = piece.moves
     current_location = piece.location
     piece_moves.each do |move|
       x = current_location[0] + move[0]
       y = current_location[1] + move[1]
-      # byebug
       next if out_of_bounds?(x,y)
       if free_space?(piece, x, y)
-      # byebug
       valid_moves << [x, y] #fix
       vector_array = check_direction(piece, x, y, move[0], move[1])
       valid_moves << vector_array unless piece.multiple_moves == false || vector_array == []
@@ -79,17 +78,13 @@ class Board
 
   def check_direction(piece, x, y, add_x, add_y, array_direction = [])
     if out_of_bounds?(x + add_x, y + add_y)
-      p "Out"
       return array_direction
     elsif friendly_fire?(piece, x + add_x, y + add_y) #&& piece.name != 'knight'
-      p "friendly"
       return array_direction
     elsif free_space?(piece, x + add_x, y + add_y)
-      p "free"
       array_direction << [x + add_x, y + add_y]
       check_direction(piece, x + add_x, y + add_y, add_x, add_y, array_direction )
     else
-      p "opponent"
       array_direction << [x + add_x, y + add_y]
     end
     array_direction
@@ -197,9 +192,11 @@ class Rook < Piece
 end
 
 class King < Piece
-  attr_reader :moves, :multiple_moves
+  attr_reader :color
+  attr_accessor :moves, :multiple_moves
   def initialize(location, color = "white")
     @location = location
+    @color = color
     @color =="white" ? @icon = "♚" : @icon = '♔'
     @multiple_moves = false
     @moves = [[0, 1],[0, -1],[1,0],[-1,0],[1,1],[-1,1],[1,-1],[-1,-1]]
@@ -209,9 +206,11 @@ end
 
 
 class Queen < Piece
-  attr_reader :moves
+  attr_reader :color
+  attr_accessor :moves, :multiple_moves
   def initialize(location, color = "white")
     @location = location
+    @color = color
     @color == "white" ? @icon = "♛" : @icon = '♕'
     @moves = [[1, 0], [1,1], [1, -1], [0, -1], [0, 1], [-1, 0], [-1, 1], [-1, -1]]
     @name = "queen"
@@ -219,9 +218,11 @@ class Queen < Piece
 end
 
 class Bishop < Piece
-  attr_reader :moves
+  attr_reader :color
+  attr_accessor :moves, :multiple_moves
   def initialize(location, color = "white")
     @location = location
+    @color = color
     @color == "white" ? @icon = "♝" : @icon = '♗'
     @moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
     @name = "bishop"
@@ -229,9 +230,11 @@ class Bishop < Piece
 end
 
 class Knight < Piece
-  attr_reader :moves, :multiple_moves
+  attr_reader :color
+  attr_accessor :moves, :multiple_moves
   def initialize(location, color = "white")
     @location = location
+    @color = color
     @color == "white" ? @icon = "♞" : @icon = '♘'
     @multiple_moves = false
     @moves = [[1, 2], [1, -2], [2, 1], [2, -1], [-1, 2], [-1, -2], [-2, 1], [-2, -1]]
@@ -244,9 +247,11 @@ b = Board.new
 # p b.board
 
 
-b.move([1,0], [2,5], b.board[1][0])
-b.move([7,7], [3,4], b.board[7][7])
-p b.board[3][4]
-p b.valid_move(b.board[3][4])
-
+ b.move([1,3], [3,3], b.board[1][3])
+ b.move([1,4], [3,4], b.board[1][4])
+ b.move([1,5], [3,5], b.board[1][5])
+#b.move([7,7], [3,4], b.board[7][7])
+# p b.board[3][4]
+# p b.valid_move(b.board[3][4])
+p b.valid_move(b.board[0][3])
 puts b.display
