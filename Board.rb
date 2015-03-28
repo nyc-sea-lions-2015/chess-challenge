4681fe7894d3586c93930a2607060c01d269896e
Alexander Mihalopoulos <amihalop@gmail.com>
2015-03-28 14:52:31 -0400

    add capture

--- Board.rb
+++ Board.rb
@@ -10,6 +10,7 @@ class Board
     @boardlength = 8
     @display_board = ""
     @board_values = { "a1"=> [0,0], "b1"=> [0,1], "c1"=> [0,2], "d1" => [0,3], "e1" => [0,4], "f1" => [0,5], "g1" => [0,6], "h1" => [0,7], "a2" => [1,0], "b2" => [1,1], "c2" => [1,2], "d2" => [1,3], "e2" => [1,4], "f2" => [1,5], "g2" => [1,6], "h2" => [1,7], "a3" => [2,0], "b3" => [2,1], "c3" => [2,2], "d3" => [2,3], "e3" => [2,4], "f3" => [2,5], "g3" => [2,6], "h3" => [2,7], "a4" => [3,0], "b4" => [3,1], "c4" => [3,2], "d4" => [3,3], "e4" => [3,4], "f4" => [3,5], "g4" => [3,6], "h4" => [3,7], "a5" => [4,0], "b5" => [4,1], "c5" => [4,2], "d5" => [4,3], "e5" => [4,4], "f5" => [4,5], "g5" => [4,6], "h5" => [4,7], "a6" => [5,0], "b6" => [5,1], "c6" => [5,2], "d6" => [5,3], "e6" => [5,4], "f6" => [5,5], "g6" => [5,6], "h6" => [5,7], "a7" => [6,0], "b7" => [6,1], "c7" => [6,2], "d7" => [6,3], "e7" => [6,4], "f7" => [6,5], "g7" => [6,6], "h7" => [6,7], "a8" => [7,0], "b8" => [7,1], "c8" => [7,2], "d8" => [7,3], "e8" => [7,4], "f8" => [7,5], "g8" => [7,6], "h8" => [7,7] }
+    @captured = []
   end
 
   def display
@@ -59,6 +60,15 @@ class Board
     (x < 0 || y < 0 || x > 7|| y > 7)
   end
 
+  def piece_captured?(piece, location)
+    (square(location[0], location[1]) != nil)
+  end
+
+  def capture_piece(location)
+    @captured << square(location[0], location[1])
+    square(location[0], location[1]) = nil
+  end
+
   def valid_move(piece)
     valid_moves = []
     piece_moves = piece.moves
@@ -98,7 +108,7 @@ class Board
   def find_piece(location)
     piece = square(location[0], location[1])
   end
-
+end
 
 class Piece
   attr_accessor :color, :moves, :location, :name
@@ -225,12 +235,10 @@ b = Board.new
 # p b.board
 
 
- b.move([1,3], [3,3], b.board[1][3])
-b.move([1,4], [3,4], b.board[1][4])
-b.move([1,5], [3,5], b.board[1][5])
-b.move([6,1], [5,1], b.board[6][1])
-# p b.board[3][4]
-# p b.valid_move(b.board[3][4])
-p b.valid_move(b.board[5][1])
-#puts b.display
-p b.board[1][1]
+#  b.move(b.board[1][3], [3,3])
+
+# # p b.board[3][4]
+# # p b.valid_move(b.board[3][4])
+# p b.valid_move(b.board[1][1])
+# #puts b.display
+# p b.board[1][1]
