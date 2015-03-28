ca18ad9902b7b7755a76501a7ef17aeb2821259f
costolo <ben.costolo@gmail.com>
2015-03-28 12:54:40 -0400

    create variables for kings and add additional tests for check_mate?

--- chess_controller.rb
+++ chess_controller.rb
@@ -5,23 +5,15 @@ game.set_up_board
 game.to_s
 
 def get_value(chosen_piece) #user input string
-  conversion = {
-  "a" => 0,
-  "b" => 1,
-  "c" => 2,
-  "d" => 3,
-  "e" => 4,
-  "f" => 5,
-  "g" => 6,
-  "h" => 7}
+  conversion = {a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7}
   coordinate = [chosen_piece[1].to_i - 1, conversion[chosen_piece[0]]]
 end
 
-  puts "white's turn"
-  puts "white, your move?"
-  chosen_piece = gets.chomp.downcase 
-  coords = get_value(chosen_piece)
-  p coords
-  p game.coordinate_to_object(coords)
+  # puts "white's turn"
+  # puts "white, your move?"
+  # chosen_piece = gets.chomp.downcase 
+  # coords = get_value(chosen_piece)
+  # p coords
+  # p game.coordinate_to_object(coords)
 
 
